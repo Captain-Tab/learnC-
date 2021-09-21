07fe82aa84deaf3442fef338dc2eef234890d59e
captain-tab <d491405879@gmail.com>
2021-09-21 10:45:18 +0800

    feat: add input

---
+++ input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // float annualSalary;
+  // float monethlySalary = annualSalary / 12;
+  // cout << "Your monthly salary is" << monethlySalary;
+  float annualSalary;
+  cout << "Please enter your annual salary";
+  cin >> annualSalary;
+  float monthlySalary = annualSalary / 12;
+  cout << "Your monthly salary is" << monthlySalary << endl;
+  cout << "In 10 years you will earn" << annualSalary * 10;
+
+  char character = 'a';
+  
+}
