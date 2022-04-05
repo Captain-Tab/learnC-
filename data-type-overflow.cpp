da1f5caee7f7f23c793f62b7a85bf8c644dc15b4
captain-tab <d491405879@gmail.com>
2022-04-05 10:49:51 +0800

    feat: add data type overflow

---
+++ data-type-overflow.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int intMax = INT_MAX;
+  cout << intMax << endl;
+  cout << intMax + 1;
+  
+  // it goest to the minimum when over the max size of int
+  // it calls the date type overflow
+
+}
